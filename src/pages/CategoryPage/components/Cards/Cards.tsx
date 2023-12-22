631b904b1d0fdc62a76a5a3af53f0b58f8b588e0
Poghos Barseghyan <bpoghos94@gmail.com>
2023-12-22 23:46:08 +0400

    category page cards hover

--- src/pages/CategoryPage/components/Cards/Cards.tsx
+++ src/pages/CategoryPage/components/Cards/Cards.tsx
@@ -1,40 +1,23 @@
-import { Button, Col, Container, Row } from "react-bootstrap"
+import { Button, Container, Row } from "react-bootstrap"
 import styles from "./Cards.module.css"
+import Card from "./Card/Card"
+import photo1 from "./Card/test/photo_1.jpg"
+import photo2 from "./Card/test/photo_2.jpeg"
+import photo3 from "./Card/test/photo_3.jpeg"
 
 const Cards: React.FC = () => {
     return (
         <>
             <Container className={styles.container}>
                 <Row>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
-                    <Col lg={4}>
-                        <div className={styles.card}></div>
-                    </Col>
+                    <Card test={photo1} />
+                    <Card test={photo2} />
+                    <Card test={photo3} />
+                    <Card test={photo3} />
+                    <Card test={photo2} />
+                    <Card test={photo1} />
                 </Row>
-                <Button>MORE</Button>
+                <Button variant="secondary">MORE</Button>
             </Container>
         </>
     )
